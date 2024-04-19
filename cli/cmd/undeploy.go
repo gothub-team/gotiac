bc6af4dccb9751af773814628c014bfe50232036
Vincenz Koop <vincenz.koop@gmail.com>
2024-04-19 10:18:14 +0200

    added descriptions to commands

--- cli/cmd/undeploy.go
+++ cli/cmd/undeploy.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,8 @@ import (
 // undeployCmd represents the undeploy command
 var undeployCmd = &cobra.Command{
 	Use:   "undeploy",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Undeploy all resources of a given feature from the cloud provider.",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("undeploy called")
 	},
