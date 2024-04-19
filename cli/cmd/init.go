f94dae3f47b4eb6f7e7499022ec6168fd582b08b
Vincenz Koop <vincenz.koop@gmail.com>
2024-04-19 11:59:15 +0200

    created base selection

--- cli/cmd/init.go
+++ cli/cmd/init.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	// "strconv"
 
-	// "github.com/manifoldco/promptui"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 
 	"gothub-team/gotiac/features"
@@ -25,29 +25,57 @@ var initCmd = &cobra.Command{
 		fmt.Println(`Welcome to gotiac! Let's create the infrastructure for your got project.`)
 		fmt.Println()
 
-		items, err := util.SelectItems(`Select the features you want to initiate.`, 0, []*util.Item{
-			{ID: "CDN"},
-			{ID: "Storage"},
-		})
-
-		// Label:     "Select the features you want to initiate.",
-
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			return
-		}
-
-		for _, item := range items {
-			switch item.ID {
-			case "CDN":
-				features.Cdn()
-			case "Storage":
-				features.Storage()
-			default:
-				fmt.Printf("Unknown feature: %s\n", item.ID)
-			}
-		}
+		initProject()
+		initFeatures()
 	},
+}
+
+func initProject() {
+	stagePrompt := promptui.Prompt{
+		Label: "Enter the stage name you want to deploy to",
+	}
+	awsProfilePrompt := promptui.Prompt{
+		Label: "Enter the AWS profile you want to use",
+	}
+
+	regions := []string{"us-east-1", "us-west-1", "us-west-2", "eu-west-1", "eu-central-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "sa-east-1"}
+	awsRegionSelect := promptui.Select{
+		Label:     "Enter the AWS region you want to use",
+		Items:     regions,
+		CursorPos: 4,
+	}
+
+	stage, _ := stagePrompt.Run()
+	awsProfile, _ := awsProfilePrompt.Run()
+	regionIdx, _, _ := awsRegionSelect.Run()
+	awsRegion := regions[regionIdx]
+
+	fmt.Printf("Initializing stage %s for profile %s in %s \n", stage, awsProfile, awsRegion)
+}
+
+func initFeatures() {
+	items, err := util.SelectItems(`Select the features you want to initiate.`, 0, []*util.Item{
+		{ID: "CDN"},
+		{ID: "Storage"},
+	})
+
+	// Label:     "Select the features you want to initiate.",
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	for _, item := range items {
+		switch item.ID {
+		case "CDN":
+			features.Cdn()
+		case "Storage":
+			features.Storage()
+		default:
+			fmt.Printf("Unknown feature: %s\n", item.ID)
+		}
+	}
 }
 
 func init() {
