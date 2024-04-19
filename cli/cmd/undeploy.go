cc072ea7c2178b4d87f55f297b7baac9acc456da
Vincenz Koop <vincenz.koop@gmail.com>
2024-04-19 10:12:10 +0200

    initial gotiac go CLI app

---
+++ cli/cmd/undeploy.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// undeployCmd represents the undeploy command
+var undeployCmd = &cobra.Command{
+	Use:   "undeploy",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("undeploy called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(undeployCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// undeployCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// undeployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
